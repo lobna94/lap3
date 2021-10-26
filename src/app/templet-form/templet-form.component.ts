4924ca770b00eefa6e538626bf8b3487af2603f4
lobna94 <lobnahassan499@gmail.com>
2021-10-26 18:55:49 +0200

    lab

--- src/app/templet-form/templet-form.component.ts
+++ src/app/templet-form/templet-form.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-templet-form',
-  templateUrl: './templet-form.component.html',
-  styleUrls: ['./templet-form.component.css']
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
 })
-export class TempletFormComponent implements OnInit {
+export class TemplateFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
   }
-
   submitForm(form:any){
-    console.log(form);
+    console.log(form.form.controls.useremail.errors)
+  }
+  inchange(useremail:any){
+    console.log(useremail)
+  }
+  onclick(){
+    this.router.navigate([`forms/template-reactive`]);
   }
 }
